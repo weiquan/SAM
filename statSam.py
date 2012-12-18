4f745d052a09771e8be5c9fa46fbd3c054b01d19
Wei Quan <wquanhit@gmail.com>
2012-12-18 17:31:30 +0800

    add the percent

--- statSam.py
+++ statSam.py
@@ -17,26 +17,34 @@ def main(argv):
     if fp_sam == False:
         print fn_sam + " file open fail!"
         exit(1)
+    totReads = fp_sam.mapped + fp_sam.unmapped;
     statMappedNum = fp_sam.mapped;
     statUnMappedNum = fp_sam.unmapped; 
     statPerfectHit = 0;
     statGoodHit = 0
     for read in fp_sam.fetch():
+        #parse pos answer
         realPos = [0, 0]
-        realPos[0] = int(read.qname.split('_')[2]);#parse pos answer
+        realPos[0] = int(read.qname.split('_')[2]);
         realPos[1] = int(read.qname.split('_')[3]);
+        #end pos to start pos
+        if realPos[1] > realPos[0]:
+            realPos[1] = realPos[1] - read.qlen
+        else:
+            realPos[0] = realPos[0] - read.qlen
         
-        realPos[1] = realPos[1] - read.qlen
-        pos = read.pos +1#0-based to 1-based
+        pos = read.pos +1#0-based leftmost pos to 1-based leftmost pos
         if  pos in realPos:
             statPerfectHit = statPerfectHit +1;
         else:
-            if pos in read.positions:
+            if realPos[0] in read.positions or realPos[1] in read.positions:
                 statGoodHit = statGoodHit +1;
-    print "Num of Mapped Reads: %d"%(statMappedNum);
-    print "Num of Unmapped Reads: %d"%(statUnMappedNum);
-    print "Num of Perfect Hits: %d"%(statPerfectHit);
-    print "Num of Good Hits: %d"%(statGoodHit);
+
+    print "Num of total Reads: %d"%(totReads);
+    print "Num of Mapped Reads: %d, percent %f %%"%(statMappedNum, float(statMappedNum)/float(totReads) * 100);
+    print "Num of Unmapped Reads: %d, percent %f %%"%(statUnMappedNum, float(statUnMappedNum)/float(totReads) * 100);
+    print "Num of Perfect Hits: %d, percent %f %%"%(statPerfectHit, float(statPerfectHit)/float(totReads) * 100);
+    print "Num of Good Hits: %d, percent %f %%"%(statGoodHit, float(statGoodHit)/float(totReads) * 100);
         
     fp_sam.close();
 
